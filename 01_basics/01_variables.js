9d38d546b6f4710f69baa7c847d7c3277cfd4e20
lagukav <kavyaklakshmi@gmail.com>
2024-09-26 17:04:33 +0000

    variables topic changes done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,7 @@
+const accoutNumber =12345
+let accountPwd='12345@kav'
+var accontEmail="Kavgmail"
+
+// accoutNumber=12367
+console.log(accoutNumber)
+console.table([accontEmail,accountPwd,accoutNumber])
